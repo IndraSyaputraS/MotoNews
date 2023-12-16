b89f035c0fd5bfb77cad696b6e13911cfde72280
Indra <syaput123@gmail.com>
2023-12-16 22:09:55 +0700

    [Bab 3] State dan Props

---
+++ data.js
@@ -0,0 +1,140 @@
+export const CategoryList = [
+    {
+      id: 1,
+      categoryName: 'Formula 1',
+    },
+    {
+      id: 2,
+      categoryName: 'Rally',
+    },
+    {
+      id: 3,
+      categoryName: 'WEC',
+    },
+    {
+      id: 4,
+      categoryName: 'moto GP',
+    },
+    {
+      id: 5,
+      categoryName: 'Formula E',
+    },
+    {
+      id: 6,
+      categoryName: 'Indycar',
+    },
+    {
+      id: 7,
+      categoryName: 'Nascar',
+    },
+  ];
+  export const BlogList = [
+    {
+      id: 1,
+      title: 'Sauber Konfirmasi Nama Tim Baru untuk 2024 dan 2025',
+      category: 'Formula 1',
+      image:
+        'https://cdn.crash.net/styles/medium_article/s3/pa/3524070.0064.jpg',
+      createdAt: 'Dec 15, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 2,
+      title: 'Tujuh mobil balap kelas dunia tampil di Danau Toba Rally 2023',
+      category: 'Rally',
+      image:
+        'https://img.antaranews.com/cache/1200x800/2023/06/21/DANAU-TOBA-RALLY-2023.jpg.webp',
+      createdAt: 'Nov 10, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 3,
+      title: 'Valentino Rossi Kini Jadi Pembalap Mobil di Kejuaraan WEC 2024',
+      category: 'WEC',
+      image:
+        'https://imgcdn.solopos.com/@space/2023/07/rsz1207rossi.jpg',
+      createdAt: 'Nov 10, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 4,
+      title: 'Martin Memang Lebih Cepat, tapi Bagnaia Bisa Atasi Tekanan',
+      category: 'Moto GP',
+      image:
+        'https://akcdn.detik.net.id/community/media/visual/2023/11/27/jorge-martin_169.jpeg?w=700&q=90',
+      createdAt: 'Nov 10, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 5,
+      title: 'Jakarta Absen dari Agenda Formula E 2024',
+      category: 'Formula E',
+      image:
+        'https://images.bisnis.com/posts/2023/11/23/1717347/formula-e.jpg',
+      createdAt: 'Nov 10, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 6,
+      title: 'Sting Ray Robb Racing for 2024 IndyCar season',
+      category: 'Indycar',
+      image:
+        'https://cdn-7.motorsport.com/images/amp/2GzbzyE0/s1200/sting-ray-robb-dale-coyne-raci.webp',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 7,
+      title: 'Jenson Button Gabung NASCAR Garage 56',
+      category: 'Nascar',
+      image:
+        'https://cdn-1.motorsport.com/images/amp/01KjavoY/s1200/lemans-24-hours-of-le-mans-201.webp',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 8,
+      title: 'Hamilton Bisa Patahkan Dominasi Verstappen',
+      category: 'Formula 1',
+      image:
+        'https://riaupos.co/thumb/4869-10-lewis-hamilton.jpg',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 9,
+      title: 'Hamilton Minta Mercedes Tak Tiru Red Bull',
+      category: 'Formula 1',
+      image:
+        'https://cdn-2.motorsport.com/images/amp/0ZRab5o0/s1200/lewis-hamilton-mercedes-f1-w14.webp',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 10,
+      title: 'Red Bull Harus Bayar Mahal Pendaftaran F1 2024',
+      category: 'Formula 1',
+      image:
+        'https://cdn-9.motorsport.com/images/amp/2wBdKdb0/s1200/fireworks-light-the-sky-as-the.webp',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 11,
+      title: 'Aston Martin Berlomba di Le Mans 24 Jam 2025',
+      category: 'WEC',
+      image:
+        'https://cdn.crash.net/styles/medium_article/s3/field/image/Screenshot%202023-10-05%20074557.png',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 12,
+      title: 'Pisah dengan Honda Solusi Terbaik untuk Dua Pihak',
+      category: 'Moto GP',
+      image:
+        'https://cdn-7.motorsport.com/images/amp/0rG3P9q2/s1200/marc-marquez-repsol-honda-team.webp',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+  ];
